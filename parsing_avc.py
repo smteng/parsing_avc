e55ad7a07d25724d407f9b19d09ed9fd1a3d540e
ShihMeng Teng <Meng_Teng@pegatroncorp.com>
2021-03-04 16:45:41 +0800

    Add new condition for checking

--- parsing_avc.py
+++ parsing_avc.py
@@ -11,5 +11,10 @@ for line in lines:
         if len(items[0]) == 6:
             tmp['allow '+items[0][2].split(':')[2]+' '+items[0][3].split(':')[2]+':'+items[0][4]+' {'+items[0][0]+'}'] = 0
 
+    items = re.findall('avc:  denied  {(.*)}(.*)scontext=(.*) tcontext=(.*) tclass=(.*) permissive=(.*)',line)
+    if len(items) > 0:
+        if len(items[0]) == 6:
+            tmp['allow '+items[0][2].split(':')[2]+' '+items[0][3].split(':')[2]+':'+items[0][4]+' {'+items[0][0]+'}'] = 0
+
 for key in sorted(tmp.keys()):
-    print key+';'
+    print(key+';')
